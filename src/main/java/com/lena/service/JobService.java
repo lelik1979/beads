f04837747ebbd253be4844e4b3c98b9cf3333a24
Alexey Dranchuk <alexey.dranchuk@gmail.com>
2014-11-25 02:20:49 +0200

    enable email job

--- src/main/java/com/lena/service/JobService.java
+++ src/main/java/com/lena/service/JobService.java
@@ -18,7 +18,7 @@ public class JobService {
 
     private static final Logger LOG  = LoggerFactory.getLogger(JobService.class);
 
-//    @Scheduled(cron="${email.job.schedule}")
+    @Scheduled(cron="${email.job.schedule}")
     public void processOrders() {
         emailJobService.processPendingOrders();
     }
