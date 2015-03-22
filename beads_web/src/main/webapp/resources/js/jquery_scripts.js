b3d0ef98a0ead866ba3bb631f27e60a394b80466
Alexey Dranchuk <alexey.dranchuk@opower.com>
2015-03-22 22:21:09 +0200

    extract common code to thymeleaf template

--- beads_web/src/main/webapp/resources/js/jquery_scripts.js
+++ beads_web/src/main/webapp/resources/js/jquery_scripts.js
@@ -1,5 +1,73 @@
-jQuery(document).ready(function($) {
+function addToBasket(button) {
+    var id = button.attr('id');
+    console.log("send /addItemToOrder with id=" + id);
 
-    $('#description').html("This is updated by jQuery")
+    $.ajax({
+        url: "/addItemToOrder",
+        type: "PUT",
+        contentType: "application/json; charset=utf-8",
+        data: '{"productId":' + id + '}',
+        success: function () {
+            console.log("Success add to basket");
+            button.attr('disabled', 'true');
+        },
+        error: function () {
+            console.log("Error add to basket");
+        }
+    });
+}
 
-});
+function removeEmailError() {
+    $("#email_error").hide();
+}
+
+function checkEmail() {
+    var email = $("#email_field").val();
+    var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
+
+    if (filter.test(email)) {
+        removeEmailError();
+        $("#Order_button").removeAttr('disabled')
+    } else {
+        $("#email_error").show();
+        $("#Order_button").attr('disabled','disabled')
+    }
+}
+
+function deleteProduct(button) {
+    console.log("Delete productId=" + button.attr('productId'));
+    var productId = button.attr('productId');
+    $.ajax({
+        url: "/deleteItemFromBasket",
+        type: "PUT",
+        contentType: "application/json; charset=utf-8",
+        data: '{"productId":' + productId + '}',
+        success: function () {
+            console.log("successfully deleted product");
+            location.reload(true);
+        },
+        error: function () {
+            console.log("Failed to remove product from basket");
+        }
+    });
+}
+
+function showSubmitOrderForm() {
+    $("#submitOrder").show();
+    $("#makeOrder").prop("disabled", "true");
+}
+
+function makeOrder(button) {
+    $.ajax({
+        url: "/submitOrder",
+        type: "PUT",
+        contentType: "application/json; charset=utf-8",
+        data: ' ',
+        success: function () {
+            console.log("successfully created order");
+        },
+        error: function () {
+            console.log("Error add to basket");
+        }
+    });
+}
