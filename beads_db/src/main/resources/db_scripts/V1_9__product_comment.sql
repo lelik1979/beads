5f2cfbcaefbcac688040562d60de03a6a60500a2
alexandr <alexandr.makarevich88@gmail.com>
2018-02-23 23:03:58 +0200

    corrected the code according to the requirements of the collaborator

--- beads_db/src/main/resources/db_scripts/V1_9__product_comment.sql
+++ beads_db/src/main/resources/db_scripts/V1_9__product_comment.sql
@@ -1,16 +1,16 @@
 CREATE TABLE `product_comment` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `product_id` INT NOT NULL,
-  `comment` VARCHAR(4000) NULL DEFAULT NULL,
+  `comment` VARCHAR(4000) NOT NULL,
   `disadvantages` VARCHAR(400) NULL DEFAULT NULL,
-  `dignity` VARCHAR(400) NULL DEFAULT NULL,
+  `advantage` VARCHAR(400) NULL DEFAULT NULL,
   `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` ENUM('VISIBLE', 'INVISIBLE') NULL DEFAULT 'INVISIBLE',
-  `rating` INT NULL,
+  `status` ENUM('VISIBLE', 'INVISIBLE') NOT NULL DEFAULT 'INVISIBLE',
+  `rating` TINYINT NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
-  INDEX `index_product_comment` (`product_id` ASC),
-  CONSTRAINT `fk_product_comment`
+  INDEX `index_product_id` (`product_id` ASC),
+  CONSTRAINT `fk_product_id`
     FOREIGN KEY (`product_id`)
     REFERENCES `product` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+ )
--- beads_model/src/main/java/com/beads/model/domain/ProductComment.java
+++ beads_model/src/main/java/com/beads/model/domain/ProductComment.java
@@ -2,19 +2,18 @@ package com.beads.model.domain;
 
 import com.beads.model.constant.CommentStatus;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
 
 @Entity
 @Table(name="product_comment")
@@ -25,7 +24,7 @@ public class ProductComment {
   @Column(name = "id")
   private Integer id;
 
-  @OneToOne
+  @OneToOne(fetch= FetchType.LAZY)
   @JoinColumn(name = "product_id")
   private Product product;
 
@@ -35,8 +34,8 @@ public class ProductComment {
   @Column(name = "disadvantages")
   private String disadvantages;
 
-  @Column(name = "dignity")
-  private String dignity;
+  @Column(name = "advantages")
+  private String advantages;
 
   @Column(name = "create_date", columnDefinition = "timestamp")
   private LocalDateTime createDate = LocalDateTime.now();
@@ -80,12 +79,12 @@ public class ProductComment {
     this.disadvantages = disadvantages;
   }
 
-  public String getDignity() {
-    return dignity;
+  public String getAdvantages() {
+    return advantages;
   }
 
-  public void setDignity(String dignity) {
-    this.dignity = dignity;
+  public void setAdvantages(String advantages) {
+    this.advantages = advantages;
   }
 
   public LocalDateTime getCreateDate() {
@@ -115,34 +114,13 @@ public class ProductComment {
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
-
     if (o == null || getClass() != o.getClass()) return false;
-
     ProductComment that = (ProductComment) o;
-
-    return new EqualsBuilder()
-        .append(id, that.id)
-        .isEquals();
+    return Objects.equals(id, that.id);
   }
 
   @Override
   public int hashCode() {
-    return new HashCodeBuilder(17, 37)
-        .append(id)
-        .toHashCode();
-  }
-
-  @Override
-  public String toString() {
-    return new ToStringBuilder(this)
-        .append("id", id)
-        .append("product", product)
-        .append("comment", comment)
-        .append("disadvantages", disadvantages)
-        .append("dignity", dignity)
-        .append("createDate", createDate)
-        .append("status", status)
-        .append("rating", rating)
-        .toString();
+    return Objects.hash(id);
   }
 }
