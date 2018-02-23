715c91c60f34d3d933b67d80dfbfa15f952bd20a
alexandr <alexandr.makarevich88@gmail.com>
2018-02-23 19:20:27 +0200

    changes have been made to display the style of the letter to the manager

--- beads_email/src/main/java/com/beads/email/config/EmailSenderConfig.java
+++ beads_email/src/main/java/com/beads/email/config/EmailSenderConfig.java
@@ -19,7 +19,7 @@ import java.util.Properties;
  */
 
 @Configuration
-@PropertySource("email/${env}.properties")
+@PropertySource("email/${env:dev}.properties")
 public class EmailSenderConfig {
 
     @Autowired
