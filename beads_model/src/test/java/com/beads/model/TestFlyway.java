1556e80669f7b201a423d4a4499eba8ff1fbd693
alexandr <alexandr.makarevich88@gmail.com>
2018-02-26 19:39:20 +0200

    added test for flyway

---
+++ beads_model/src/test/java/com/beads/model/TestFlyway.java
@@ -0,0 +1,12 @@
+package com.beads.model;
+
+import com.beads.model.dao.CommonDaoIT;
+import org.junit.Test;
+
+public class TestFlyway extends CommonDaoIT {
+
+  @Test
+  public void test() {
+
+  }
+}
