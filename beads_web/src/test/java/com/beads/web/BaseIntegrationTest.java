aea30640d0616d24ff4b691a957c92924ebf0e26
AlexandrMakarevich <alexandr.makarevich88@gmail.com>
2018-03-21 14:51:39 +0200

    fixed test configuration property

--- beads_web/src/test/java/com/beads/web/BaseIntegrationTest.java
+++ beads_web/src/test/java/com/beads/web/BaseIntegrationTest.java
@@ -1,13 +1,13 @@
 package com.beads.web;
 
-import com.beads.model.config.BaseIntegrationTestConfiguration;
+import com.beads.web.configuration.IntegrationTestConfig;
 import org.junit.runner.RunWith;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ComponentScan(basePackages = {"com.beads.web.dao", "com.beads.model.builder"})
+@ContextConfiguration(classes = IntegrationTestConfig.class)
 @Transactional
-public abstract class BaseIntegrationTest extends BaseIntegrationTestConfiguration {
+public abstract class BaseIntegrationTest {
 }
